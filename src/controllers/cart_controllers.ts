839882ac58e83e8a069f8ee272ec88a1beeace25
Au3 <aum3523@gmail.com>
2025-01-24 13:33:40 +0700

    Fix Cart controller

--- src/controllers/cart_controllers.ts
+++ src/controllers/cart_controllers.ts
@@ -1,17 +1,35 @@
 import express from 'express';
 import { createCart, deleteCartById, getCart, getCartById, updateCartById } from '../models/cart';
 import { getProductById } from '../models/product';
+import { resolve } from 'path';
+import { rejects } from 'assert';
 
 export const newCart = async (req: express.Request , res: express.Response): Promise<any> => {
     try {
-        // const products: Array<any> = req.body.products;
-        // var total_price: Awaited<number> = 0;
+        const products: Array<any> = req.body.products;
 
-        // // const cart = await createCart({
-        // //     products,
-        // //     total_price
-        // // });
+        // const cart = await createCart({
+        //     products, 
+        // });
+        const length = products.length;
 
+        const fetchData = async (): Promise<any> => {
+            return new Promise((resolve , rejects) => {
+                setTimeout(() => {
+                    var total_price = 0;
+                    products.forEach(async (values: any, index: number)=>{
+                        const product = await getProductById(values.product_Id);
+                        total_price += product.product_price * values.amount;
+                        if(index == length-1){
+                            resolve(total_price);
+                        }
+                    });
+                }, 2000);              
+            });
+        }
+
+        const total = await fetchData();
+        console.log(total);
         
         // var updated_cart;
         // const length = products.length;
@@ -21,13 +39,13 @@ export const newCart = async (req: express.Request , res: express.Response): Pro
         //     total_price += product.product_price * values.amount;
         //     console.log(total_price);
         //     if(index == length-1){
-        //         // total_price = total_price - (total_price * ((length-1)*10) / 100);
+        //         total_price = total_price - (total_price * ((length-1)*10) / 100);
         //         console.log(total_price);
         //         console.log(index);
-        //         // updated_cart = await updateCartById(cart._id.toString() , {products,total_price});
+        //         updated_cart = await updateCartById(cart._id.toString() , {products,total_price});
         //     }
         // });
-        // // const get_cart = await getCartById(cart._id.toString());
+        // const get_cart = await getCartById(cart._id.toString());
         
         return res.sendStatus(200);
     } catch (error) {
