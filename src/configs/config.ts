ec28861af08447be99626b901102bd2c5c196dfd
Au3 <aum3523@gmail.com>
2025-01-27 16:10:52 +0700

    config

---
+++ src/configs/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    PORT : 8080,
+    MONGO_URL : "mongodb+srv://aum3523:01233210-Asd@back-end-submission-db.er1vb.mongodb.net/?retryWrites=true&w=majority&appName=back-end-submission-db"
+}
