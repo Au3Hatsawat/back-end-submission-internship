4a749397d6ffde8e7e446841d6d515ff4c778f50
Au3 <aum3523@gmail.com>
2025-01-27 14:05:35 +0700

    swagger api-doc

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,41 @@ import mongoose from 'mongoose';
 import { config } from './configs/config';
 import { error } from 'console';
 import router from './router';
+import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc';
 
 const app = express();
 app.use(cors({
     credentials: true
 }));
 
+const Options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'My Express API',
+        version: '1.0.0',
+        description: 'This is a simple CRUD API application',
+      },
+      servers: [
+        {
+          url: 'http://localhost:8080',
+          description: 'Development server',
+        },
+      ],
+    },
+    apis: ['./src/router/*.ts'], // ระบุเส้นทางของไฟล์ที่มีการกำหนด API
+  };
+  
+  
+  const swaggerSpec = swaggerJSDoc(Options);
+  console.log(JSON.stringify(swaggerSpec, null, 2));
+
+
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 
 const server = http.createServer(app);
 const PORT = config.PORT;
--- src/router/product_route.ts
+++ src/router/product_route.ts
@@ -2,8 +2,67 @@ import express from 'express';
 import { newProduct , getAll , updateProduct , deleteProduct, getById, getByName, getProductPerPage, getProductCategoryPerPage, getProductInCategory} from '../controllers/product_controllers';
 
 export default (router: express.Router) => {
-    // Get method to read products...
+ /**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Retrieve a list of products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: A list of products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                      type: ObjectId
+ *                      example: 6794a8a6e3331cc566d8c0ab
+ *                   product_name:
+ *                     type: string
+ *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
+ *                   product_price:
+ *                     type: number
+ *                     example: 100
+ *                   category:
+ *                      type: string
+ *                      example: Science Fiction & Fantasy
+ *                  
+ */
     router.get('/products/' , getAll);
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Retrieve a product by _id
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: A product by _id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                      type: ObjectId
+ *                      example: 6794a8a6e3331cc566d8c0ab
+ *                   product_name:
+ *                     type: string
+ *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
+ *                   product_price:
+ *                     type: number
+ *                     example: 100
+ *                   category:
+ *                      type: string
+ *                      example: Science Fiction & Fantasy
+ *                  
+ */
     router.get('/products/:_id' , getById);
     router.get('/products/name/:product_name' , getByName);
     router.get('/products/pages/:_page' , getProductPerPage);
