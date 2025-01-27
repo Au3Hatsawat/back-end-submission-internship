64b56817e39416fca65a8922df063861e954f7bb
Au3 <aum3523@gmail.com>
2025-01-27 15:48:12 +0700

    swagger api-doc

--- src/router/cart_route.ts
+++ src/router/cart_route.ts
@@ -1,9 +1,211 @@
 import express from 'express';
-import { addProductsToCart, deleteCart, getAll, newCart} from '../controllers/cart_controllers';
+import { addProductsToCart, deleteCart, getAll, newCart } from '../controllers/cart_controllers';
 
 export default (router: express.Router) => {
-    router.get('/carts/' , getAll);
-    router.post('/carts/new' , newCart);
-    router.delete('/carts/delete/:_id' , deleteCart);
-    router.put('/carts/add/:_id' , addProductsToCart);
+   /**
+* @swagger
+* /carts:
+*   get:
+*     summary: Retrieve a list of carts
+*     tags: [Carts]
+*     responses:
+*       200:
+*         description: A list of carts
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*                 properties:
+*                   _id:
+*                      type: ObjectId
+*                      example: 6793a31f9cf1913a6f4bb228
+*                   total_price:
+*                     type: number
+*                     example: 400
+*                   products:
+*                     type: array
+*                     items:
+*                      type: object
+*                      properties:
+*                          product_Id: 
+*                              type: ObjectId
+*                              example: 6791f9deef767dc9dd1a4344
+*                          amount: 
+*                              type: number
+*                              example: 4
+*                          _id:
+*                              type: ObjectId
+*                              example: 6793a31f9cf1913a6f4bb22a
+*                   
+*                  
+*/
+   router.get('/carts/', getAll);
+   /**
+   * @swagger
+   * /carts/new:
+   *   post:
+   *     summary: Create a new cart
+   *     tags: [Carts]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               products:
+   *                 type: array
+   *                 items:
+   *                   type: object
+   *                   properties:
+   *                     product_Id:
+   *                       type: string
+   *                       example: "6791fa6eef767dc9dd1a4351"
+   *                     amount:
+   *                       type: number
+   *                       example: 4
+   *     responses:
+   *       200:
+   *         description: Cart successfully created
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 _id:
+   *                   type: string
+   *                   example: "6793a31f9cf1913a6f4bb228"
+   *                 total_price:
+   *                   type: number
+   *                   example: 400
+   *                 products:
+   *                   type: array
+   *                   items:
+   *                     type: object
+   *                     properties:
+   *                       product_Id:
+   *                         type: string
+   *                         example: "6791fa6eef767dc9dd1a4351"
+   *                       amount:
+   *                         type: number
+   *                         example: 4
+   *                       _id:
+   *                         type: string
+   *                         example: "6793a31f9cf1913a6f4bb22a"
+   */
+   router.post('/carts/new', newCart);
+   /**
+ * @swagger
+ * /carts/delete/{id}:
+ *   delete:
+ *     summary: Delete a cart by ID
+ *     tags: [Carts]
+ *     parameters:
+ *       - in: path
+ *         name: cart_id
+ *         schema:
+ *           type: ObjectId
+ *         required: true
+ *         description: The ID of the cart to delete
+ *     responses:
+ *       200:
+ *         description: Cart successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                      type: ObjectId
+ *                      example: 6793a31f9cf1913a6f4bb228
+ *                   total_price:
+ *                     type: number
+ *                     example: 400
+ *                   products:
+ *                     type: array
+ *                     items:
+ *                      type: object
+ *                      properties:
+ *                          product_Id: 
+ *                              type: ObjectId
+ *                              example: 6791f9deef767dc9dd1a4344
+ *                          amount: 
+ *                              type: number
+ *                              example: 4
+ *                          _id:
+ *                              type: ObjectId
+ *                              example: 6793a31f9cf1913a6f4bb22a
+ *                   
+ *                  
+ */
+   router.delete('/carts/delete/:_id', deleteCart);
+   /**
+ * @swagger
+ * /carts/add/{id}:
+ *   put:
+ *     summary: Add a product to cart by product_Id
+ *     tags: [Carts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               products:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     product_Id:
+ *                       type: string
+ *                       example: "6791f9deef767dc9dd1a4344"
+ *                     amount:
+ *                       type: number
+ *                       example: 4
+ *     parameters:
+ *       - in: path
+ *         name: product_Id
+ *         schema:
+ *           type: ObjectId
+ *         required: true
+ *         description: The ID of the product that you will add to cart
+ *     responses:
+ *       200:
+ *         description: Product successfully added
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                      type: ObjectId
+ *                      example: 6793a31f9cf1913a6f4bb228
+ *                   total_price:
+ *                     type: number
+ *                     example: 400
+ *                   products:
+ *                     type: array
+ *                     items:
+ *                      type: object
+ *                      properties:
+ *                          product_Id: 
+ *                              type: ObjectId
+ *                              example: 6791f9deef767dc9dd1a4344
+ *                          amount: 
+ *                              type: number
+ *                              example: 4
+ *                          _id:
+ *                              type: ObjectId
+ *                              example: 6793a31f9cf1913a6f4bb22a
+ *                   
+ *                  
+ */
+   router.put('/carts/add/:_id', addProductsToCart);
 };
--- src/router/product_route.ts
+++ src/router/product_route.ts
@@ -1,78 +1,424 @@
 import express from 'express';
-import { newProduct , getAll , updateProduct , deleteProduct, getById, getByName, getProductPerPage, getProductCategoryPerPage, getProductInCategory} from '../controllers/product_controllers';
+import { newProduct, getAll, updateProduct, deleteProduct, getById, getByName, getProductPerPage, getProductCategoryPerPage, getProductInCategory } from '../controllers/product_controllers';
 
 export default (router: express.Router) => {
- /**
- * @swagger
- * /products:
- *   get:
- *     summary: Retrieve a list of products
- *     tags: [Products]
- *     responses:
- *       200:
- *         description: A list of products
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   _id:
- *                      type: ObjectId
- *                      example: 6794a8a6e3331cc566d8c0ab
- *                   product_name:
- *                     type: string
- *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
- *                   product_price:
- *                     type: number
- *                     example: 100
- *                   category:
- *                      type: string
- *                      example: Science Fiction & Fantasy
- *                  
- */
-    router.get('/products/' , getAll);
-/**
- * @swagger
- * /products/{id}:
- *   get:
- *     summary: Retrieve a product by _id
- *     tags: [Products]
- *     responses:
- *       200:
- *         description: A product by _id
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   _id:
- *                      type: ObjectId
- *                      example: 6794a8a6e3331cc566d8c0ab
- *                   product_name:
- *                     type: string
- *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
- *                   product_price:
- *                     type: number
- *                     example: 100
- *                   category:
- *                      type: string
- *                      example: Science Fiction & Fantasy
- *                  
- */
-    router.get('/products/:_id' , getById);
-    router.get('/products/name/:product_name' , getByName);
-    router.get('/products/pages/:_page' , getProductPerPage);
-    router.get('/products/categories/:category/:_page' , getProductCategoryPerPage);
-    router.get('/products/categories/:category' , getProductInCategory);
-    // Post method to create new product...
-    router.post('/products/add' , newProduct);
-    // Put method to update product...
-    router.put('/products/update/:_id' , updateProduct);
-    // Delete method to delete product...
-    router.delete('/products/delete/:_id' , deleteProduct);
+    /**
+    * @swagger
+    * /products:
+    *   get:
+    *     summary: Retrieve a list of products
+    *     tags: [Products]
+    *     responses:
+    *       200:
+    *         description: A list of products
+    *         content:
+    *           application/json:
+    *             schema:
+    *               type: array
+    *               items:
+    *                 type: object
+    *                 properties:
+    *                   _id:
+    *                     type: string
+    *                     example: "6794a8a6e3331cc566d8c0ab"
+    *                   product_name:
+    *                     type: string
+    *                     example: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+    *                   product_price:
+    *                     type: number
+    *                     example: 100
+    *                   category:
+    *                     type: string
+    *                     example: "Science Fiction & Fantasy"
+    *               example:
+    *                 - _id: "6794a8a6e3331cc566d8c0ab"
+    *                   product_name: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+    *                   product_price: 100
+    *                   category: "Science Fiction & Fantasy"
+    *                 - _id: "6794b2d1f9cf1913a6f4bb229"
+    *                   product_name: "The Hobbit"
+    *                   product_price: 200
+    *                   category: "Fantasy"
+    *                 - _id: "6794c7d8f2ab1913a6f4bb22c"
+    *                   product_name: "The Catcher in the Rye"
+    *                   product_price: 150
+    *                   category: "Classic Literature"
+    */
+    router.get('/products/', getAll);
+    /**
+     * @swagger
+     * /products/{id}:
+     *   get:
+     *     summary: Retrieve a product by id
+     *     tags: [Products]
+     *     parameters:
+     *       - in: path
+     *         name: product_Id
+     *         schema:
+     *           type: ObjectId
+     *         required: true
+     *         description: The ID of the product that you will get
+     *     responses:
+     *       200:
+     *         description: A product by id
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   _id:
+     *                      type: ObjectId
+     *                      example: 6794a8a6e3331cc566d8c0ab
+     *                   product_name:
+     *                     type: string
+     *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
+     *                   product_price:
+     *                     type: number
+     *                     example: 100
+     *                   category:
+     *                      type: string
+     *                      example: Science Fiction & Fantasy
+     *                  
+     */
+    router.get('/products/:_id', getById);
+    /**
+     * @swagger
+     * /products/name/{name}:
+     *   get:
+     *     summary: Retrieve a product by name
+     *     tags: [Products]
+     *     parameters:
+     *       - in: path
+     *         name: product_Name
+     *         schema:
+     *           type: string
+     *         required: true
+     *         description: The name of the product that you will get
+     *     responses:
+     *       200:
+     *         description: A product by name
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   _id:
+     *                      type: ObjectId
+     *                      example: 6794a8a6e3331cc566d8c0ab
+     *                   product_name:
+     *                     type: string
+     *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
+     *                   product_price:
+     *                     type: number
+     *                     example: 100
+     *                   category:
+     *                      type: string
+     *                      example: Science Fiction & Fantasy
+     *                  
+     */
+    router.get('/products/name/:product_name', getByName);
+    /**
+     * @swagger
+     * /products/page/{page}:
+     *   get:
+     *     summary: Retrieve a list of products by page
+     *     tags: [Products]
+     *     parameters:
+     *       - in: path
+     *         name: page
+     *         schema:
+     *           type: number
+     *         required: true
+     *         description: The page number of the products to retrieve
+     *         example: 1
+     *     responses:
+     *       200:
+     *         description: A list of products
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   _id:
+     *                     type: string
+     *                     example: "6794a8a6e3331cc566d8c0ab"
+     *                   product_name:
+     *                     type: string
+     *                     example: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *                   product_price:
+     *                     type: number
+     *                     example: 100
+     *                   category:
+     *                     type: string
+     *                     example: "Science Fiction & Fantasy"
+     *               example:
+     *                 - _id: "6794a8a6e3331cc566d8c0ab"
+     *                   product_name: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *                   product_price: 100
+     *                   category: "Science Fiction & Fantasy"
+     *                 - _id: "6794b2d1f9cf1913a6f4bb229"
+     *                   product_name: "The Hobbit"
+     *                   product_price: 200
+     *                   category: "Fantasy"
+     *                 - _id: "6794c7d8f2ab1913a6f4bb22c"
+     *                   product_name: "The Catcher in the Rye"
+     *                   product_price: 150
+     *                   category: "Classic Literature"
+     */
+    router.get('/products/pages/:_page', getProductPerPage);
+    /**
+     * @swagger
+     * /products/categories/{category}/{page}:
+     *   get:
+     *     summary: Retrieve a list of products by page
+     *     tags: [Products]
+     *     parameters:
+     *       - in: path
+     *         name: category
+     *         schema:
+     *           type: string
+     *         required: true
+     *         description: The category of product that you will get
+     *         example: Science Fiction & Fantasy
+     *       - in: path
+     *         name: page
+     *         schema:
+     *         type: number
+     *         required: true
+     *         description: The page number of the products to retrieve
+     *         example: 1 
+     *     responses:
+     *       200:
+     *         description: A list of products
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   _id:
+     *                     type: string
+     *                     example: "6794a8a6e3331cc566d8c0ab"
+     *                   product_name:
+     *                     type: string
+     *                     example: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *                   product_price:
+     *                     type: number
+     *                     example: 100
+     *                   category:
+     *                     type: string
+     *                     example: "Science Fiction & Fantasy"
+     *               example:
+     *                 - _id: "6794a8a6e3331cc566d8c0ab"
+     *                   product_name: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *                   product_price: 100
+     *                   category: "Science Fiction & Fantasy"
+     *                 - _id: "6794a8c5e3331cc566d8c0ad"
+     *                   product_name: "แฮร์รี่ พอตเตอร์ กับห้องแห่งความลับ เล่ม 2"
+     *                   product_price: 100
+     *                   category: "Science Fiction & Fantasy"
+     *                 - _id: "6794a8e1e3331cc566d8c0af"
+     *                   product_name: "แฮร์รี่ พอตเตอร์ กับนักโทษแห่งอัซคาบัน เล่ม 3"
+     *                   product_price: 100
+     *                   category: "Science Fiction & Fantasy"
+     */
+    router.get('/products/categories/:category/:_page', getProductCategoryPerPage);
+    /**
+     * @swagger
+     * /products/categories/{category}:
+     *   get:
+     *     summary: Retrieve a list of products by page
+     *     tags: [Products]
+     *     parameters:
+     *       - in: path
+     *         name: category
+     *         schema:
+     *           type: string
+     *         required: true
+     *         description: The category of product that you will get
+     *         example: Science Fiction & Fantasy
+     *     responses:
+     *       200:
+     *         description: A list of products
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   _id:
+     *                     type: string
+     *                     example: "6794a8a6e3331cc566d8c0ab"
+     *                   product_name:
+     *                     type: string
+     *                     example: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *                   product_price:
+     *                     type: number
+     *                     example: 100
+     *                   category:
+     *                     type: string
+     *                     example: "Science Fiction & Fantasy"
+     *               example:
+     *                 - _id: "6794a8a6e3331cc566d8c0ab"
+     *                   product_name: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *                   product_price: 100
+     *                   category: "Science Fiction & Fantasy"
+     *                 - _id: "6794a8c5e3331cc566d8c0ad"
+     *                   product_name: "แฮร์รี่ พอตเตอร์ กับห้องแห่งความลับ เล่ม 2"
+     *                   product_price: 100
+     *                   category: "Science Fiction & Fantasy"
+     *                 - _id: "6794a8e1e3331cc566d8c0af"
+     *                   product_name: "แฮร์รี่ พอตเตอร์ กับนักโทษแห่งอัซคาบัน เล่ม 3"
+     *                   product_price: 100
+     *                   category: "Science Fiction & Fantasy"
+     */
+    router.get('/products/categories/:category', getProductInCategory);
+    /**
+     * @swagger
+     * /products/add:
+     *   post:
+     *     summary: Add a new product
+     *     tags: [Products]
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               product_name:
+     *                 type: string
+     *                 example: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *               product_price:
+     *                 type: number
+     *                 example: 100
+     *               category:
+     *                 type: string
+     *                 example: "Science Fiction & Fantasy"
+     *     responses:
+     *       200:
+     *         description: product that you added
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   _id:
+     *                      type: ObjectId
+     *                      example: 6794a8a6e3331cc566d8c0ab
+     *                   product_name:
+     *                     type: string
+     *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
+     *                   product_price:
+     *                     type: number
+     *                     example: 100
+     *                   category:
+     *                      type: string
+     *                      example: Science Fiction & Fantasy
+     *                  
+     */
+    router.post('/products/add', newProduct);
+    /**
+     * @swagger
+     * /products/update/{id}:
+     *   put:
+     *     summary: Update a product
+     *     tags: [Products]
+     *     parameters:
+     *       - in: path
+     *         name: product_Id
+     *         schema:
+     *           type: ObjectId
+     *         required: true
+     *         description: The ID of the product that you will update
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               product_name:
+     *                 type: string
+     *                 example: "แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1"
+     *               product_price:
+     *                 type: number
+     *                 example: 100
+     *               category:
+     *                 type: string
+     *                 example: "Science Fiction & Fantasy"
+     *     responses:
+     *       200:
+     *         description: Update your product
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   _id:
+     *                      type: ObjectId
+     *                      example: 6794a8a6e3331cc566d8c0ab
+     *                   product_name:
+     *                     type: string
+     *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
+     *                   product_price:
+     *                     type: number
+     *                     example: 100
+     *                   category:
+     *                      type: string
+     *                      example: Science Fiction & Fantasy
+     *                  
+     */
+    router.put('/products/update/:_id', updateProduct);
+    /**
+    * @swagger
+    * /products/delete/{id}:
+    *   delete:
+    *     summary: Delete your product
+    *     tags: [Products]
+    *     parameters:
+    *       - in: path
+    *         name: product_Id
+    *         schema:
+    *           type: ObjectId
+    *         required: true
+    *         description: The ID of the product that you will delete
+    *     responses:
+    *       200:
+    *         description: Delete your product that you added
+    *         content:
+    *           application/json:
+    *             schema:
+    *               type: array
+    *               items:
+    *                 type: object
+    *                 properties:
+    *                   _id:
+    *                      type: ObjectId
+    *                      example: 6794a8a6e3331cc566d8c0ab
+    *                   product_name:
+    *                     type: string
+    *                     example: แฮร์รี่ พอตเตอร์ กับศิลาอาถรรพ์ เล่ม 1
+    *                   product_price:
+    *                     type: number
+    *                     example: 100
+    *                   category:
+    *                      type: string
+    *                      example: Science Fiction & Fantasy
+    *                  
+    */
+    router.delete('/products/delete/:_id', deleteProduct);
 };
 
